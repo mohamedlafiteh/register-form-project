100a6595a3d62385cc42f2b029663fbf16551a51
mohamedlafiteh <m_lafiteh@yahoo.com>
2021-04-22 02:32:38 +0100

    Change the submit title style

--- src/components/steps/Submit.js
+++ src/components/steps/Submit.js
@@ -5,7 +5,7 @@ import Container from "@material-ui/core/Container"
 export const Submit = () => {
     return (
         <Container maxWidth="sm" style= {{marginTop:"5rem"}}>
-            <h1>Thanks for submitting the from</h1>
+            <h2 style={{textAlign:"center"}}>Thanks for submitting the form</h2>
         </Container>
     )
 }
